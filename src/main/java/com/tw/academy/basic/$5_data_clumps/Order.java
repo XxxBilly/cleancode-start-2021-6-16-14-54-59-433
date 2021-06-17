961a02b28f28fca3cf60491c5e31b8d09c9b24f6
Billy Mai <billy.mai@oocl.com>
2021-06-17 16:11:30 +0800

    extract user feild

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -2,23 +2,25 @@ package com.tw.academy.basic.$5_data_clumps;
 
 public class Order {
 
-    private int orderNumber;
+  private final User user;
+  private int orderNumber;
 
-    private String buyerName;
-    private String buyerPhoneNumber;
-    private String buyerAddress;
+  private String buyerName;
+  private String buyerPhoneNumber;
+  private String buyerAddress;
 
-    public Order(int orderNumber, User user) {
-        this.orderNumber = orderNumber;
+  public Order(int orderNumber, User user) {
+    this.orderNumber = orderNumber;
 
-        this.buyerName = user.getBuyerName();
-        this.buyerPhoneNumber = user.getBuyerPhoneNumber();
-        this.buyerAddress = user.getBuyerAddress();
-    }
+    this.user = user;
+    this.buyerName = this.user.getBuyerName();
+    this.buyerPhoneNumber = this.user.getBuyerPhoneNumber();
+    this.buyerAddress = this.user.getBuyerAddress();
+  }
 
-    public String confirm(){
-        return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
-    }
+  public String confirm() {
+    return String.format("Please confirm buyer information: buyer name is %s, " +
+        "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+  }
 }
 
